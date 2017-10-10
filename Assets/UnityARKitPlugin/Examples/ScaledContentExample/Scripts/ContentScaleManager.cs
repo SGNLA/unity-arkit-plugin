2b5332e18ea3f8d266b6fbe84694030e1e56a0da
Jimmy Alamparambil <jimmy@unity3d.com>
2017-10-10 11:36:02 -0700

    Two camera system that works by keeping content static, but moving content camera in such a way as to make content appear at right scale, position and rotation.

--- Assets/UnityARKitPlugin/Examples/ScaledContentExample/Scripts/ContentScaleManager.cs
+++ Assets/UnityARKitPlugin/Examples/ScaledContentExample/Scripts/ContentScaleManager.cs
@@ -4,33 +4,25 @@ using UnityEngine;
 
 public class ContentScaleManager : MonoBehaviour {
 
-	public delegate void ContentScaleChanged(Vector3 newScale, Vector3 prevScale); 
+	public delegate void ContentScaleChanged(float newScale, float prevScale); 
 	public static ContentScaleChanged ContentScaleChangedEvent;
 
 	[SerializeField]
-	private Vector3 m_ContentScale = Vector3.one;
-
-	private Touch m_TouchZoom0;
-	private Touch m_TouchZoom1;
-	private float m_LastTouchDistance;
-	public float m_ZoomSpeed = 0.5f;
+	private float m_ContentScale = 1.0f;
 
 	void Start()
 	{
-		Vector3 prevScale = gameObject.transform.localScale;
-		gameObject.transform.localScale = m_ContentScale;
 		if (ContentScaleChangedEvent != null)
-			ContentScaleChangedEvent(m_ContentScale, prevScale);
+			ContentScaleChangedEvent(m_ContentScale, 1.0f);
 	}
 	
-	public Vector3 ContentScale {
+	public float ContentScale {
 		get { return m_ContentScale; }
 		set { 
 			if (value != m_ContentScale)
 			{
-				Vector3 prevScale = m_ContentScale;
-				m_ContentScale = new Vector3(Mathf.Clamp(value.x, 1.0f, 100.0f), Mathf.Clamp(value.y, 1.0f, 100.0f), Mathf.Clamp(value.z, 1.0f, 100.0f));
-				gameObject.transform.localScale = value;
+				float prevScale = m_ContentScale;
+				m_ContentScale = Mathf.Clamp(value, 0.001f, 1000.0f);
 				if (ContentScaleChangedEvent != null)
 				{
 					ContentScaleChangedEvent(m_ContentScale, prevScale);
