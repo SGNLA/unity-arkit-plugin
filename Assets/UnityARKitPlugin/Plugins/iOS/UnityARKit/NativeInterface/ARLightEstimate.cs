3364e0bce0e4516f89ea01781ddbb3b592e6dc4f
Jimmy Alamparambil <jimmy@unity3d.com>
2017-11-02 11:27:29 -0700

    Correct spherical harmonics coefficients for Unity handedness when marshaling Put six cameras around sphere to capture lighting from the different directions

--- Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/NativeInterface/ARLightEstimate.cs
+++ Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/NativeInterface/ARLightEstimate.cs
@@ -34,10 +34,44 @@ namespace UnityEngine.XR.iOS
 			int numFloats = 27;
 			float [] workCoefficients = new float[numFloats];
 			Marshal.Copy (ptrFloats, workCoefficients, 0, (int)(numFloats)); 
-
+			RotateForUnity (ref workCoefficients, 0);
+			RotateForUnity (ref workCoefficients, 9);
+			RotateForUnity (ref workCoefficients, 18);
 			return workCoefficients;
+
 		}
 
+		void RotateForUnity(ref float[] shc, int startIndex)
+		{
+			//from observation, it looks like top, bottom and left,right are flipped (x,y)
+
+			// we got this info:
+			// Lets assume Unity uses the same convention for spherical harmonics (yzx) wrt its own coordinate system as scenekit. 
+			// (This could be another source of error).
+			// To tackle a constant transform between the unity and scenekit world coordinate system conventions:
+
+			// Axis flips work as follows for the 9 components of the sh vector:
+
+			//	0-component: no change
+
+			//	1-component: negate if y is supposed to be flipped
+			//	2-component: negate if z is supposed to be flipped
+			//	3-component: negate if x is supposed to be flipped
+
+			//	4-component: negate if x or y are supposed to be flipped, but not both!
+			//	5-component: negate if y or z are supposed to be flipped, but not both!
+			//	6-component: no change
+			//	7-component: negate if x or z are supposed to be flipped, but not both!
+			//	8-component: no change
+
+			shc [startIndex+1] = -shc [startIndex+1];
+			shc [startIndex+3] = -shc [startIndex+3];
+
+			shc [startIndex + 5] = -shc [startIndex + 5];				
+			shc [startIndex + 7] = -shc [startIndex + 7];
+				
+
+		}
 	}
 
 	public class UnityARDirectionalLightEstimate
